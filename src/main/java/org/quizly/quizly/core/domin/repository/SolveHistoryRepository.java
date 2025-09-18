36434187ce66ab281f0fbb5175d72f25205ed339
fnzl54 <rnjscksdud1@naver.com>
2025-09-18 16:37:52 +0900

    fix(#19): 문제 조회 api N+1 이슈 해결

--- src/main/java/org/quizly/quizly/core/domin/repository/SolveHistoryRepository.java
+++ src/main/java/org/quizly/quizly/core/domin/repository/SolveHistoryRepository.java
@@ -9,6 +9,6 @@ import org.springframework.data.repository.query.Param;
 
 public interface SolveHistoryRepository extends JpaRepository<SolveHistory, Long> {
 
-  @Query("SELECT sh FROM SolveHistory sh WHERE sh.user = :user AND (sh.quiz.id, sh.createdAt) IN (SELECT sh2.quiz.id, MAX(sh2.createdAt) FROM SolveHistory sh2 WHERE sh2.user = :user GROUP BY sh2.quiz.id)")
+  @Query("SELECT sh FROM SolveHistory sh LEFT JOIN FETCH sh.quiz WHERE sh.user = :user AND (sh.quiz.id, sh.createdAt) IN (SELECT sh2.quiz.id, MAX(sh2.createdAt) FROM SolveHistory sh2 WHERE sh2.user = :user GROUP BY sh2.quiz.id)")
   List<SolveHistory> findLatestSolveHistoriesByUser(@Param("user") User user);
 }
