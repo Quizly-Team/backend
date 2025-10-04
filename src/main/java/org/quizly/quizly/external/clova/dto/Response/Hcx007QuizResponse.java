02d840ea9dde3ea2ebfce8e82047f39d95300e96
fnzl54 <rnjscksdud1@naver.com>
2025-10-04 19:43:31 +0900

    refactor(#27): hcx-007 다양한 요청에 대응 가능하도록 구조 변경

--- src/main/java/org/quizly/quizly/external/clova/dto/Response/Hcx007QuizResponse.java
+++ src/main/java/org/quizly/quizly/external/clova/dto/Response/Hcx007QuizResponse.java
@@ -1,27 +1,19 @@
 package org.quizly.quizly.external.clova.dto.Response;
-
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.quizly.quizly.core.domin.entity.Quiz;
-import org.quizly.quizly.core.domin.entity.SolveHistory;
-
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Hcx007Response {
+public class Hcx007QuizResponse {
   private String quiz;
   private Quiz.QuizType type;
   private List<String> options;
   private String answer;
   private String explanation;
-  private LocalDateTime createdAt;
-  private LocalDateTime updatedAt;
-  private String latestSolveStatus;
   private String topic;
 }
 
