1b936d07cdacac9bcde255362a278dae13c96f3f
fnzl54 <rnjscksdud1@naver.com>
2025-11-24 10:20:27 +0900

    feat(#51): 문제 주제 생성을 별도 ai로 분리

---
+++ src/main/java/org/quizly/quizly/external/clova/dto/Response/Hcx007TopicResponse.java
@@ -0,0 +1,12 @@
+package org.quizly.quizly.external.clova.dto.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Hcx007TopicResponse {
+  private String topic;
+}
