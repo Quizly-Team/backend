a9921f4465d14dcce0c23966c30b94588ae0b75a
yeonchaepark <check4732@naver.com>
2025-12-15 18:46:59 +0900

    feat(#68): apache PDFBox+OCR 10페이지 분할 적용

---
+++ src/main/java/org/quizly/quizly/core/util/ByteArrayMultipartFile.java
@@ -0,0 +1,71 @@
+package org.quizly.quizly.core.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Optional;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class ByteArrayMultipartFile implements MultipartFile {
+
+    private final byte[] content;
+    private final String name;
+    private final String originalFilename;
+    private final String contentType;
+
+    public ByteArrayMultipartFile(
+            String name,
+            String originalFilename,
+            String contentType,
+            byte[] content
+    ) {
+        this.name = name;
+        this.originalFilename = originalFilename;
+        this.contentType = contentType;
+        this.content = Optional.ofNullable(content)
+                .map(arr -> arr.clone())
+                .orElse(null);
+
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getOriginalFilename() {
+        return originalFilename;
+    }
+
+    @Override
+    public String getContentType() {
+        return contentType;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return content.length == 0;
+    }
+
+    @Override
+    public long getSize() {
+        return content.length;
+    }
+
+    @Override
+    public byte[] getBytes() {
+        return content == null ? null : content.clone();
+    }
+
+    @Override
+    public InputStream getInputStream() {
+        return new ByteArrayInputStream(content);
+    }
+
+    @Override
+    public void transferTo(java.io.File dest) throws IOException {
+        throw new UnsupportedOperationException("Not supported");
+    }
+}
