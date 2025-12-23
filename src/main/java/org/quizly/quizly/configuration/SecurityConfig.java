821dabeda58465bfaa73ac538bf89ed9d4f1b99d
fnzl54 <rnjscksdud1@naver.com>
2025-12-23 20:04:00 +0900

    feat(#65): 사용자 풀이 통계 배치 수동 API (admin)

--- src/main/java/org/quizly/quizly/configuration/SecurityConfig.java
+++ src/main/java/org/quizly/quizly/configuration/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.quizly.quizly.oauth.OAuth2LoginSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -19,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
 
   private final OAuth2LoginUserService oAuth2LoginUserService;
