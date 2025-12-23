b3729c61a1b2e97b6f26aa1051260cfe143e57c8
fnzl54 <rnjscksdud1@naver.com>
2025-12-23 13:38:08 +0900

    feat(#65): 유저의 문제 유형별 일일 통계 배치 구현

---
+++ src/main/java/org/quizly/quizly/core/domin/entity/UserQuizTypeDailySummary.java
@@ -0,0 +1,39 @@
+package org.quizly.quizly.core.domin.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.quizly.quizly.core.domin.entity.Quiz.QuizType;
+import org.quizly.quizly.core.domin.shared.BaseEntity;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Entity
+@Table(name = "user_quiz_type_daily_summary", uniqueConstraints = { @UniqueConstraint(columnNames = {"user_id", "quiz_type", "summary_date"}) })
+public class UserQuizTypeDailySummary extends BaseEntity {
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private QuizType quizType;
+
+  @Column(name = "summary_date", nullable = false)
+  private LocalDate date;
+
+  @Column(nullable = false)
+  private Integer solvedCount = 0;
+
+  @Column(nullable = false)
+  private Integer correctCount = 0;
+}
