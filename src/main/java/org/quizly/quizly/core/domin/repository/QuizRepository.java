34b6fbd666896c9d1c5318ad1b5a641e765533aa
fnzl54 <rnjscksdud1@naver.com>
2026-02-19 15:03:18 +0900

    feat(#114): 인덱스 정리 및 쿼리 튜닝

--- src/main/java/org/quizly/quizly/core/domin/repository/QuizRepository.java
+++ src/main/java/org/quizly/quizly/core/domin/repository/QuizRepository.java
@@ -4,11 +4,6 @@ import java.util.List;
 import org.quizly.quizly.core.domin.entity.Quiz;
 import org.quizly.quizly.core.domin.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
-  List<Quiz> findAllByUser(User user);
-
-  @Query("SELECT DISTINCT q FROM Quiz q LEFT JOIN FETCH q.options WHERE q.user = :user")
-  List<Quiz> findAllByUserWithOptions(User user);
 }
