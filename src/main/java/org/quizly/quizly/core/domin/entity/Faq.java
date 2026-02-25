bce892f353a0c5bae4a47e3db139dce585d6f030
fnzl54 <rnjscksdud1@naver.com>
2026-02-25 15:08:28 +0900

    feat(#118): faq 등록 API 구현 (Admin API)

---
+++ src/main/java/org/quizly/quizly/core/domin/entity/Faq.java
@@ -0,0 +1,45 @@
+package org.quizly.quizly.core.domin.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.quizly.quizly.core.domin.shared.BaseEntity;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Entity
+@Table(name = "faq")
+public class Faq extends BaseEntity {
+
+  @Getter
+  @RequiredArgsConstructor
+  public enum FaqCategory {
+    SERVICE_INTRO("서비스 소개"),
+    QUIZ_GENERATION("문제 생성"),
+    WRONG_ANSWER("오답 관리"),
+    TECH_SUPPORT("기술 지원");
+
+    private final String description;
+  }
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private FaqCategory category;
+
+  @Column(nullable = false)
+  private String question;
+
+  @Column(columnDefinition = "TEXT", nullable = false)
+  private String answer;
+}
