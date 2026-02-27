8551bb36ea58b7ce26bdeda0ce1248bc75034f8b
fnzl54 <rnjscksdud1@naver.com>
2026-02-27 10:58:53 +0900

    feat(#119): faq 조회 API 구현

--- src/main/java/org/quizly/quizly/core/domin/repository/FaqRepository.java
+++ src/main/java/org/quizly/quizly/core/domin/repository/FaqRepository.java
@@ -1,7 +1,13 @@
 package org.quizly.quizly.core.domin.repository;
 
+import java.util.List;
 import org.quizly.quizly.core.domin.entity.Faq;
+import org.quizly.quizly.core.domin.entity.Faq.FaqCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FaqRepository extends JpaRepository<Faq, Long> {
+
+  List<Faq> findAllByDeletedFalseOrderByCreatedAt();
+
+  List<Faq> findByCategoryAndDeletedFalseOrderByCreatedAt(FaqCategory category);
 }
