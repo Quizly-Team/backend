0ccd87fa591912d813e31e4767f224c152f3f3e5
fnzl54 <rnjscksdud1@naver.com>
2026-02-27 13:42:10 +0900

    feat(#120): faq 삭제 API 구현

---
+++ src/main/java/org/quizly/quizly/admin/service/DeleteFaqService.java
@@ -0,0 +1,78 @@
+package org.quizly.quizly.admin.service;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import lombok.extern.log4j.Log4j2;
+import org.quizly.quizly.admin.service.DeleteFaqService.DeleteFaqRequest;
+import org.quizly.quizly.admin.service.DeleteFaqService.DeleteFaqResponse;
+import org.quizly.quizly.core.application.BaseRequest;
+import org.quizly.quizly.core.application.BaseResponse;
+import org.quizly.quizly.core.application.BaseService;
+import org.quizly.quizly.core.domin.repository.FaqRepository;
+import org.quizly.quizly.core.exception.DomainException;
+import org.quizly.quizly.core.exception.error.BaseErrorCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class DeleteFaqService implements BaseService<DeleteFaqRequest, DeleteFaqResponse> {
+
+  private final FaqRepository faqRepository;
+
+  @Override
+  @Transactional
+  public DeleteFaqResponse execute(DeleteFaqRequest request) {
+    if (request == null || !request.isValid()) {
+      return DeleteFaqResponse.builder()
+          .success(false)
+          .errorCode(DeleteFaqErrorCode.NOT_FOUND_FAQ)
+          .build();
+    }
+
+    return faqRepository.findByIdAndDeletedFalse(request.getFaqId())
+        .map(faq -> {
+          faq.softDelete();
+          DeleteFaqResponse response = DeleteFaqResponse.builder().build();
+          return response;
+        })
+        .orElse(DeleteFaqResponse.builder()
+            .success(false)
+            .errorCode(DeleteFaqErrorCode.NOT_FOUND_FAQ)
+            .build());
+  }
+
+  @Getter
+  @RequiredArgsConstructor
+  public enum DeleteFaqErrorCode implements BaseErrorCode<DomainException> {
+
+    NOT_FOUND_FAQ(HttpStatus.NOT_FOUND, "FAQ를 찾을 수 없습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    @Override
+    public DomainException toException() {
+      return new DomainException(httpStatus, this);
+    }
+  }
+
+  @Getter
+  @Builder
+  public static class DeleteFaqRequest implements BaseRequest {
+    private Long faqId;
+
+    @Override
+    public boolean isValid() {
+      return faqId != null;
+    }
+  }
+
+  @Getter
+  @SuperBuilder
+  public static class DeleteFaqResponse extends BaseResponse<DeleteFaqErrorCode> {
+  }
+}
