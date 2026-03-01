94d4a7305d49f95cf0798f9444e7f1ed064bed7a
yeonchaepark <check4732@naver.com>
2026-03-01 18:34:05 +0900

    feat(#127): 관리자 문의 답변 등록 추가

--- src/main/java/org/quizly/quizly/core/domin/repository/InquiryRepository.java
+++ src/main/java/org/quizly/quizly/core/domin/repository/InquiryRepository.java
@@ -5,6 +5,7 @@ import org.quizly.quizly.core.domin.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface InquiryRepository extends JpaRepository<Inquiry,Long> {
     List<Inquiry> findAllByUser(User user);
